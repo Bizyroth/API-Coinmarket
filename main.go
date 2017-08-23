80141387548f12416100ecee0301a1d8b1556b55
Bizyroth <bizyroth@gmail.com>
2017-08-23 14:46:35 +0200

    Conversion in double

--- main.go
+++ main.go
@@ -3,63 +3,63 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	//"os"
-	"net/http"
 	"io/ioutil"
+	"net/http"
 	"time"
 )
 
-
-
-type API struct{
-	Id					string `json:"id"`
-	Name				string `json:"name"`
-	Symbol				string `json:"symbol"`
-	Rank				string `json:"rank"`
-	Price_usd 			string `json:"price_usd"`
-	Price_btc			string `json:"price_btc"`
-	Vol_usd				string `json:"24h_volume_usd"`
-	Market_cap_usd		string `json:"market_cap_usd"`
-	Available_supply	string `json:"available_supply"`
-	Total_supply		string `json:"total_supply"`
-	Percent_change_1h	string `json:"percent_change_1h"`
-	Percent24h			string `json:"percent_change_24h"`
-	Percent7d			string `json:"percent_change_7d"`
-	Last_updated		string `json:"last_updated"`
-	Price_eur			string `json:"price_eur"`
-	Vol_eur		 		string `json: "24h_volume_eur"`
-	Market_cap_eur		string `json:"market_cap_eur"`
-
+type aPI struct {
+	ID              string  `json:"id"`
+	Name            string  `json:"name"`
+	Symbol          string  `json:"symbol"`
+	Rank            string  `json:"rank"`
+	PriceUsd        float64 `json:"price_usd"`
+	PriceBtc        string  `json:"price_btc"`
+	VolUsd          float64 `json:"24h_volume_usd"`
+	MarketCapUsd    float64 `json:"market_cap_usd"`
+	AvailableSupply float64 `json:"available_supply"`
+	TotalSupply     float64 `json:"total_supply"`
+	PercentChange1h float64 `json:"percent_change_1h"`
+	Percent24h      string  `json:"percent_change_24h"`
+	Percent7d       string  `json:"percent_change_7d"`
+	LastUpdated     float64 `json:"last_updated"`
+	PriceEur        string  `json:"price_eur"`
+	VolEur          float64 `json:"24h_volume_eur"`
+	MarketCapEur    float64 `json:"market_cap_eur"`
 }
 
-func main(){
+func main() {
 	//i := 0
-	url:="https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10"
+	url := "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10"
 
- for {
-	cli := http.Client{}
+	for {
+		cli := http.Client{}
 
-	
-	req, err:=cli.Get(url)
-	if err != nil {
-		fmt.Println("newrequest "+err.Error())
-	}
-	defer req.Body.Close()
-	
+		req, err := cli.Get(url)
+		if err != nil {
+			fmt.Println("newrequest " + err.Error())
+		}
+		defer req.Body.Close()
 
-	body, err := ioutil.ReadAll(req.Body)
-	var aa [] API
-	json.Unmarshal(body,&aa)
-	fmt.Println("                id      symbol       price(eur)         %24h              %7j")
-	fmt.Println()
-	for l:= range aa {
-	
-		//fmt.Println(string(body))
-		fmt.Printf("%20v     %3v      %15v       %6v      %10v", aa[l].Id,aa[l].Symbol,aa[l].Price_eur, 	aa[l].Percent24h, aa[l].Percent7d)
+		body, err := ioutil.ReadAll(req.Body)
+		var aa []aPI
+		json.Unmarshal(body, &aa)
+		fmt.Printf("%20s     %5s      %15s       %6s      %10s      %10s", "ID", "symbol", "price(eur)", "%24", "%7", "price (btc)")
 		fmt.Println()
-	}
-	time.Sleep(5 * time.Minute)
-}		
+		for l := range aa {
 
-	
+			//fmt.Println(string(body))
+			priceEU, _ := strconv.ParseFloat(aa[l].PriceEur, 64)
+			percent24, _ := strconv.ParseFloat(aa[l].Percent24h, 64)
+			percent7, _ := strconv.ParseFloat(aa[l].Percent7d, 64)
+			priceBt, _ := strconv.ParseFloat(aa[l].PriceBtc, 64)
+
+			fmt.Printf("%20v     %5v      %15v       %6v      %10v      %10v", aa[l].ID, aa[l].Symbol, priceEU, percent24, percent7, priceBt)
+			fmt.Println()
+		}
+		time.Sleep(5 * time.Minute)
+	}
+
 }
