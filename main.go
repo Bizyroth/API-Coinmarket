c5367ab6fdf6e6091adce7bffaf4cf1ee71d7b60
Bizyroth <bizyroth@gmail.com>
2017-08-23 17:11:10 +0200

    coversion of price of eos on ether

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+
 )
 
 type aPI struct {
@@ -32,9 +33,9 @@ type aPI struct {
 
 func main() {
 	//i := 0
-	url := "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10"
-
+	var aa []aPI
 	for {
+		url := "https://api.coinmarketcap.com/v1/ticker/eos/?convert=EUR"
 		cli := http.Client{}
 
 		req, err := cli.Get(url)
@@ -42,24 +43,43 @@ func main() {
 			fmt.Println("newrequest " + err.Error())
 		}
 		defer req.Body.Close()
-
+		
 		body, err := ioutil.ReadAll(req.Body)
-		var aa []aPI
-		json.Unmarshal(body, &aa)
-		fmt.Printf("%20s     %5s      %15s       %6s      %10s      %10s", "ID", "symbol", "price(eur)", "%24", "%7", "price (btc)")
-		fmt.Println()
-		for l := range aa {
 
+		json.Unmarshal(body, &aa)
+		eosPriceBt, _ := strconv.ParseFloat(aa[0].PriceBtc, 64)
+		fmt.Printf("price eos: %v\n", eosPriceBt)
+		
+		url = "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR"
+		req, err = cli.Get(url)
+		if err != nil {
+			fmt.Println("newrequest " + err.Error())
+		}
+		defer req.Body.Close()
+		
+		body, err = ioutil.ReadAll(req.Body)
+
+		json.Unmarshal(body, &aa)
+		ethereumPriceBt, _ := strconv.ParseFloat(aa[0].PriceBtc, 64)		
+		fmt.Printf("price ethereum: %v\n", ethereumPriceBt)
+		
+
+		/*if(aa[l].ID=="eos" || aa[l].ID=="ethereum"){
 			//fmt.Println(string(body))
 			priceEU, _ := strconv.ParseFloat(aa[l].PriceEur, 64)
 			percent24, _ := strconv.ParseFloat(aa[l].Percent24h, 64)
 			percent7, _ := strconv.ParseFloat(aa[l].Percent7d, 64)
 			priceBt, _ := strconv.ParseFloat(aa[l].PriceBtc, 64)
-
+			
 			fmt.Printf("%20v     %5v      %15v       %6v      %10v      %10v", aa[l].ID, aa[l].Symbol, priceEU, percent24, percent7, priceBt)
 			fmt.Println()
+			
+			
+			
+			}
 		}
+*/		fmt.Printf("Price EOS in ethereum: %v\n",eosPriceBt/ethereumPriceBt)
 		time.Sleep(5 * time.Minute)
-	}
-
+		}
 }
+
